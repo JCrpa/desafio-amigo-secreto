e998db4e13e02ac338b33d06433d1ed3e7548942
Joao Costa <joaocosta.info@gmail.com>
2025-02-18 21:34:42 -0300

    criar uma função para adicionar amigos à lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,2 +1,19 @@
 //criar uma lista (array) para armazenar os nomes dos amigos
 let amigos = [];
+//criar uma função para adicionar amigos à lista
+function adicionarAmigo() {
+  //capturar o valor de entrada do input com id amigo
+  const amigoInput = document.getElementById("amigo");
+  const amigo = amigoInput.value;
+  //validar a entrada e verificar se o campo não está vazio
+  if (amigo === "") {
+      alert("Por favor, insira um nome.");
+      return; // Impede que o código continue se o campo estiver vazio
+  }
+  //se o valor for válido, adicione o nome do amigo ao array usando o método push()
+  amigos.push(amigo);
+  //redefina o campo de texto para uma string vazia
+  amigoInput.value = "";
+  //recebe a lista de amigos e atualiza a lista de amigos na tela
+  atualizarlistaAmigos(amigos);
+}
